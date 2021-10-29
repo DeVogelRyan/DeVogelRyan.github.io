c9401345abe12fba46b6a09b45c3dfe30ac8498a
DeVogelRyan <devogelryan2@gmail.com>
2021-10-29 17:00:08 +0200

    added navbar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+
+    var i = 0;
+    var txt = "Hello!";
+    var speed = 100;
+
+    function typeWriter(element, text) {
+       if (i < text.length) {
+         document.getElementById(`${element}`).innerHTML += text.charAt(i);
+         i++;
+         setTimeout(typeWriter, speed, element, text);
+       }
+      
+    }
+    setTimeout(typeWriter, speed, "typewriter-placeholder", txt);
+    
+  }
